0a197d99b154a1a318bf5d93768b410ea68adbaf
Mayur Patel <90918984+mayur035@users.noreply.github.com>
2024-12-19 10:12:16 +0530

    Update data-source.ts

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+import { FormEntity } from "./entities/form.entity";
 dotenv.config();
 
 export const DataSources = new DataSource({
@@ -11,8 +12,12 @@ export const DataSources = new DataSource({
   password: process.env.DATABASE_USERPWD,
   synchronize: false,
   database: "form_database",
-  schema:"form_schema",
-  entities: [process.env.NODE_ENV === "production" ? "./dist/entities/**/*{.js,.ts}" : "./src/entities/**/*{.ts,.js}"],
-  migrations: [process.env.NODE_ENV === "production" ? "./dist/migrations/*{.js,.ts}" : "./src/migrations/*{.ts,.js}"],
+  schema: "form_schema",
+  entities: [FormEntity],
+  migrations: [
+    process.env.NODE_ENV === "production"
+      ? "./dist/migrations/*{.js,.ts}"
+      : "./src/migrations/*{.ts,.js}",
+  ],
   //   migrationsTableName: "migrations",
 });
